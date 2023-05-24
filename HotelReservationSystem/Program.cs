0c08ea8bee9b48d944ed00850040278771916f62
John Denver Laurian <85736112+denver0619@users.noreply.github.com>
2023-05-24 14:18:13 +0800

    folder structure

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HotelReservationSystem.Login;
 
 namespace HotelReservationSystem
 {
@@ -16,7 +17,7 @@ namespace HotelReservationSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginWindow());
         }
     }
 }
