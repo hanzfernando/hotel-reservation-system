ca90b2d67f3fbb5e066ac19bdebba4c4d6a60ace
John Denver Laurian <85736112+denver0619@users.noreply.github.com>
2023-05-30 12:50:52 +0800

    PresenterCommons

---
+++ HotelReservationSystem/PresenterCommons/IPresenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelReservationSystem.PresenterCommons
+{
+    public interface IPresenter
+    {
+        Form Form { get; set; }
+        Panel Panel { get; set; }
+    }
+}
