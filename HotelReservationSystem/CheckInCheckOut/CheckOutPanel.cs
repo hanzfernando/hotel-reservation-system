cffa0151b241a7eccea3ff6d1a1e833db8f64fa0
hanzfernando <fernandohanz23@gmail.com>
2023-06-11 17:37:07 +0800

    added checkin/checkout tabs

---
+++ HotelReservationSystem/CheckInCheckOut/CheckOutPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelReservationSystem.CheckInCheckOut
+{
+    public partial class CheckOutPanel : UserControl
+    {
+        public CheckOutPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
