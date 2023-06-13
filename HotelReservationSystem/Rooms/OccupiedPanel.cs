4c7224c047eaa0d415fe2e1394e2430fbc1090e9
John Denver Laurian <85736112+denver0619@users.noreply.github.com>
2023-06-14 02:17:29 +0800

    connected reservation to rooms and locking the checkboxes

--- HotelReservationSystem/Rooms/OccupiedPanel.cs
+++ HotelReservationSystem/Rooms/OccupiedPanel.cs
@@ -25,13 +25,15 @@ namespace HotelReservationSystem.Rooms
 
         private void OnLoad(object sender, EventArgs e)
         {
-            label1.Location = new Point((this.panel2.Width/2)-(label1.Width/2), (this.panel2.Height/4)-(label1.Height/2));
+            RoomUnitLabel.Text = "Room Unit " + _presenter.RoomUnit;
+            CustomerNameLabel.Text = _presenter.RoomDetail.CustomerName;
+            DateLabel.Text = _presenter.RoomDetail.Date;
+
+            label1.Location = new Point((this.panel2.Width / 2) - (label1.Width / 2), (this.panel2.Height / 4) - (label1.Height / 2));
             RoomUnitLabel.Location = new Point((this.panel3.Width / 2) - (RoomUnitLabel.Width / 2), (this.panel3.Height / 2) - (RoomUnitLabel.Height / 2));
             CustomerNameLabel.Location = new Point((this.panel4.Width / 2) - (CustomerNameLabel.Width / 2), (this.panel4.Height / 2) - (CustomerNameLabel.Height / 2));
             DateLabel.Location = new Point((this.panel5.Width / 2) - (DateLabel.Width / 2), (this.panel5.Height / 2) - (DateLabel.Height / 2));
             CloseButton.Location = new Point((this.panel6.Width / 2) - (CloseButton.Width / 2), (this.panel6.Height / 2) - (CloseButton.Height / 2));
-            CustomerNameLabel.Text = _presenter.RoomDetail.CustomerName;
-            DateLabel.Text = _presenter.RoomDetail.Date;
         }
 
         private void CloseButton_Click(object sender, EventArgs e)
@@ -42,7 +44,7 @@ namespace HotelReservationSystem.Rooms
 
     public interface IPresenterOccupiedPanel : IPresenter
     {
-
+        int RoomUnit { get; set; }
         RoomDetail RoomDetail { get; set; }
     }
 
@@ -50,6 +52,7 @@ namespace HotelReservationSystem.Rooms
     {
         private Form _form;
         private Panel _panel;
+        private int _roomUnit;
         private RoomDetail _roomDetail;
 
         public Form Form { get { return _form; } set { _form = value; } }
@@ -61,6 +64,11 @@ namespace HotelReservationSystem.Rooms
             set { _roomDetail = value; OnPropertyChanged(nameof(RoomDetail)); }
         }
 
+        public int RoomUnit
+        {
+            get { return _roomUnit; }
+            set { _roomUnit = value; OnPropertyChanged(nameof(RoomUnit)); }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged(string propertyName)
